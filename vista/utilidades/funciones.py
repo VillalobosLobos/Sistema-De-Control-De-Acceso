4e4097122fa6ba79727cd30128492e1ca2ab8220
Villalobitos <villalobossanchezezequiel61@gmail.com>
2024-12-03 19:34:17 -0600

    Ya hay login

--- vista/utilidades/funciones.py
+++ vista/utilidades/funciones.py
@@ -1,6 +1,18 @@
+import requests
+
+url="http://127.0.0.1:8000/login"
+
 def ingresar(usuario,contraseña):
-	print("Usuario : "+usuario)
-	print("Contraseña : "+contraseña)
+	datos={
+		"usuario":usuario,
+		"contraseña":contraseña
+	}
+	response=requests.post(url,json=datos)
+	
+	if response.text=="Correcto" and usuario[0]=='V':
+		print(response.text)
+	elif response.text=="Correcto" and usuario[0]=='A':
+		print(response.text)
 
 
 
