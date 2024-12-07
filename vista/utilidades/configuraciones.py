a3a91d76fccad180223a987f4f5176fd5d01a620
Villalobitos <villalobossanchezezequiel61@gmail.com>
2024-12-06 18:42:42 -0600

    configuraciones

---
+++ vista/utilidades/configuraciones.py
@@ -0,0 +1,12 @@
+#Configuraciones para la pantalla
+dimensiones="1090x650" #Para la entrega final usar 1090x650
+
+#Colores a usar
+verde="#058749"
+verdeFuerte="#044b29"
+verdeClaro="#03b25f"
+
+
+
+
+
