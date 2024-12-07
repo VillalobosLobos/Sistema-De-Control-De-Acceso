26ca33cf4fd9f2ea6636fd0f70f6321a0a4e466a
Villalobitos <villalobossanchezezequiel61@gmail.com>
2024-12-06 18:52:17 -0600

    ya jalo las dimensiones

--- vista/utilidades/configuraciones.py
+++ vista/utilidades/configuraciones.py
@@ -1,5 +1,5 @@
 #Configuraciones para la pantalla
-dimensiones="890x450" #Para la entrega final usar 1090x650
+dimensiones="990x550" #Para la entrega final usar 1090x650
 
 #Colores a usar
 verde="#058749"
